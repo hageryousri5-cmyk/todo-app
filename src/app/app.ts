7a0bb19322b8608d39ccb2921996ba06271f9353
hageryousri5-cmyk <hageryousri5@gmail.com>
2026-08-01 21:03:09 +0300

    update todo app files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('todo-app');
+export class AppComponent {
+  tasks: string[] = ['Learn Angular', 'Build a project'];
+
+  addTask(newTask: string) {
+    if (newTask.trim() !== '') {
+      this.tasks.push(newTask);
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
